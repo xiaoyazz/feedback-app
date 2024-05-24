a70998e16863028fcb86d837e07ea63b4b2da14f
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2024-05-24 17:00:11 -0400

    Add basic js and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
 
+// use function to create react component
+// return jsx
+// use syntax sugar to put some html into js
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // const title = 'Blog Post'
+    // const body = 'This is my blog post'
+    // const comments = [
+    //     { id: 1, text: 'Comment1' },
+    //     { id: 2, text: 'Comment2' },
+    //     { id: 3, text: 'Comment3' }
+    // ]
+
+    // const loading = false
+    // const showComments = true
+
+    // const commentBlock = (
+    //     <div className="comments">
+    //         <h3>Comments({comments.length})</h3>
+    //         <ul>
+    //             {comments.map((comment, index) => (
+    //                 <li key={index}>{comment.text}</li>
+    //             ))}
+    //         </ul>
+    //     </div>
+    // )
+
+    // if (loading) return <h1>Loading...</h1>
+
+    return (
+        // <div className="container"> {/*if I use class attribute instead of className, it will cause issue because jsx doesn't supoort all html attributes*/}
+        //     <h1>{title.toUpperCase()}</h1> {/*it looks like html but it's actually jsx*/}
+        //     <p>{body}</p>
+
+        //     {showComments ? (
+        //         <div className="comments">
+        //             <h3>Comments({comments.length})</h3>
+        //             <ul>
+        //                 {comments.map((comment, index) => (
+        //                     <li key={index}>{comment.text}</li>
+        //                 ))}
+        //             </ul>
+        //         </div>) : null}
+
+        //     {/* It is equivalent to the code below (without using ternary) */}
+        //     {showComments && commentBlock}
+
+        //     <p></p>
+        //     {Math.random() * (5 + 5)} {/* it will display math calculation result on the index page*/}
+
+
+
+        // </div> // must be wrapped up with a fragment or an html element
+        <>
+            <Header></Header>
+            <div className="container">
+                <h1>My App</h1>
+            </div>
+        </>
+
+    )
 }
 
-export default App;
+export default App

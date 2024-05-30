f8c86f9fa8788dddb97d2289a8fce10bed9075a4
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2024-05-30 15:46:41 -0400

    Add motion animation for review

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
@@ -6,13 +7,32 @@ function FeedbackList({ feedback, handleDelete }) { // destructured
     if (!feedback || feedback.length === 0) {
         return <p>No Available Feedback Yet</p>
     }
+
     return (
         <div className='feedback-list'>
-            {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}></FeedbackItem>
-            ))} {/* feedback passed as a prop */}
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.div
+                        key={item.id}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
+                        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}></FeedbackItem>
+                    </motion.div>
+                ))} {/* feedback passed as a prop */}
+            </AnimatePresence>
         </div>
     )
+
+
+    // return (
+    //     <div className='feedback-list'>
+    //         {feedback.map((item) => (
+    //             <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}></FeedbackItem>
+    //         ))} {/* feedback passed as a prop */}
+    //     </div>
+    // )
 }
 
 FeedbackList.protoTypes = {
